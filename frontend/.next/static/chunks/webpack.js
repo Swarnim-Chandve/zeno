9ba9ecf00708ea3eeaad71dc9b85df9292d9c977
Swarnim <swarnimchandve23@gmail.com>
2025-09-13 14:35:41 +0530

    Add test API route to verify deployment

--- frontend/.next/static/chunks/webpack.js
+++ frontend/.next/static/chunks/webpack.js
@@ -192,7 +192,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "383792e70d0e4a07"; }
+/******/ 		__webpack_require__.h = function() { return "26d33d79570e93a7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
